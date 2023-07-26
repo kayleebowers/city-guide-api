526737a4e9cbc203fea8a3fc0cbade26cf2a518e
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-26 21:35:46 +0000

    refactor: declare and export models in same line

--- models.js
+++ models.js
@@ -16,16 +16,12 @@ let activitySchema = mongoose.Schema({
 let userSchema = mongoose.Schema({
     Username: { type: String, required: true },
     Password: { type: String, required: true },
-    Email: { type: String, required: true },
+    Email: { type: String, required: true, lowercase: true },
     // define array types in reference to Activity schema
     Todos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Activity' }],
     Completed: [{ type: mongoose.Schema.Types.Boolean, ref: 'Activity' }]
 });
 
-//declare models
-let Activity = mongoose.model("Activity", activitySchema);
-let User = mongoose.model("User", userSchema);
-
-//export models 
-module.exports.Activity = Activity;
-module.exports.User = User;
+//declare and export models 
+module.exports = mongoose.model("Activity", activitySchema);
+module.exports = mongoose.model("User", userSchema);
