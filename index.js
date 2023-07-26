c90e05148a8f1e896e18e82810643e6bc424e552
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-26 15:11:27 +0000

    feat: add morgan logging

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express"),
+    //modules to log data, work with directories/file paths, write file streams
     morgan = require("morgan"),
-    path = require("path");
+    path = require("path"),
+    fs = require('fs');
 
 const app = express(); 
+
+//order: logging, user authentication, JSON parsing, static file serving, and app routing
+
+//create log stream
+const logStream = fs.createWriteStream(path.join(__dirname, "log.txt"), { flags: "a"});
+
+//log HTTP method, date, endpoint, req status, res length
+app.use(morgan(":method :date[web] :url :status :res[content-length]", { stream: logStream }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public/documentation.html"));
